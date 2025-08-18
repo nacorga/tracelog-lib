ca9035bc55a358e5e14d08667565b3def700a5de
Ignacio Cortes Garcia <ncg2392@gmail.com>
2025-08-18 13:04:43 +0200

    fix: enforce value as a required number in WebVitalsData and enhance validation in trackWebVital method

--- src/handlers/performance.handler.ts
+++ src/handlers/performance.handler.ts
@@ -191,7 +191,7 @@ export class PerformanceHandler extends StateManager {
   }
 
   private trackWebVital(type: WebVitalType, value?: number): void {
-    if (typeof value !== 'number') {
+    if (typeof value !== 'number' || !isFinite(value)) {
       return;
     }
 
