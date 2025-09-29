cff9e0cfddc82bdb486f8169a2af57616ae40c23
Ignacio Cortes Garcia <ncg2392@gmail.com>
2025-09-29 11:44:58 +0200

    chore: update Vitest configuration files and adjust test commands for integration testing

--- vitest.integration.config.mjs
+++ vitest.integration.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vitest/config';
 import { resolve } from 'path';
+import { fileURLToPath, URL } from 'node:url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   test: {
